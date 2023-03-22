5eb147ef316927007520a1a33876452ccf22d89e
stranik87 <tulkin.boynazarov@gmail.com>
2023-03-22 13:17:23 +0500

    while10

--- While10.py
+++ While10.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    x = 0
+    cnt = 0
+
+    while x < len(s):
+        if int(s[x])%2 == 1:
+            cnt += int( s[x])
+        x += 1
+    return cnt
+
+print(main("98421"))
