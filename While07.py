d7d2e72403b7284b15159c7c68c399dd57c4d8ad
stranik87 <tulkin.boynazarov@gmail.com>
2023-03-22 13:01:52 +0500

    while07

--- While07.py
+++ While07.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    x = 0
+    cnt = 0
+
+    while x < len(s):
+        if int( s[x])%2 == 0:
+            cnt += 1
+        x += 1
+    return cnt
+
+print(main("123456"))
