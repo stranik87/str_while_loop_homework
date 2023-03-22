94acbe0736d87e7bc6076b3c2bb5d24cd2b5246a
stranik87 <tulkin.boynazarov@gmail.com>
2023-03-22 12:08:16 +0500

    while04

--- While04.py
+++ While04.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    x = 0
+    cnt = 0
+
+    while x < len(s):
+        if s[x].isupper():
+            cnt += 1
+        x += 1
+    return cnt
+
+print(main("CodeschoolUz"))
