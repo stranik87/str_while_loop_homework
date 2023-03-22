f2f577ac80e2e6cae4671508859c28950bdaa5c7
stranik87 <tulkin.boynazarov@gmail.com>
2023-03-22 16:50:42 +0500

    while06

--- While06.py
+++ While06.py
@@ -7,3 +7,19 @@ def main(s):
     Returns:
         int: return answer
     """
+    x = 0
+    y = 0
+    cnt = 0
+   
+ 
+    
+    while x < len(s):
+        if s[x] != "a" and s[x] != "e" and s[x] != "i" and s[x] != "o" and s[x] != "u" and s[x] != "y" and s[x] != "A" and s[x] != "E" and s[x] != "I" and s[x] != "O" and s[x] != "U" and s[x] != "Y":
+            cnt += 1
+
+        
+        x += 1
+    return cnt
+
+print(main("CodeschoolUz"))
+
